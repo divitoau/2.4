4c85e253d0dd5eb02388e37b66bc44079e1c0dda
divitoau <divitoau@gmail.com>
2023-11-29 14:29:31 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,73 @@
+const {
+  S3Client,
+  ListObjectsV2Command,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+const fileUpload = require("express-fileupload");
+const fs = require("fs-extra");
+const express = require("express");
+const app = express();
+
+app.use(fileUpload());
+
+const bucketName = "my-cool-local-bucket";
+
+const s3Client = new S3Client({
+  region: "us-east-1",
+  endpoint: "http://localhost:4566",
+  forcePathStyle: true,
+});
+
+app.get("/images", (req, res) => {
+  const listObjectsParams = {
+    Bucket: bucketName,
+  };
+  s3Client
+    .send(new ListObjectsV2Command(listObjectsParams))
+    .then((listObjectsResponse) => {
+      res.send(listObjectsResponse.Contents);
+    });
+});
+
+app.get("/images/:imageName", (req, res) => {
+  fs.emptyDir("downloads");
+  const file = req.params.imageName;
+  const getObjectParams = {
+    Bucket: bucketName,
+    Key: file,
+  };
+  s3Client
+    .send(new GetObjectCommand(getObjectParams))
+    .then((getObjectResponse) => {
+      getObjectResponse.Body.pipe(fs.createWriteStream(`downloads/${file}`));
+    });
+  res.send(`${file} downloaded`);
+});
+
+app.post("/images", (req, res) => {
+  fs.emptyDir("uploads");
+  const file = req.files.image;
+  const tempPath = `uploads/${file.name}`;
+  file.mv(tempPath, (err) => {
+    res.status(500);
+  });
+  const putObjectsParams = {
+    Body: file.data,
+    Bucket: bucketName,
+    Key: file.name,
+  };
+  s3Client.send(new PutObjectCommand(putObjectsParams)).then(
+    (putObjectsResponse) => {
+      res.send(putObjectsResponse);
+    },
+    () => {
+      res.status(500);
+    }
+  );
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
+});
